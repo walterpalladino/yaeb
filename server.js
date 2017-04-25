509b2366879a74099d5f3b1af57f6a6fe1ae412c
Walter H. Palladino <walterpalladino@Walters-Mac-mini.local>
2017-04-25 14:42:18 -0300

    Some project configuration

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
